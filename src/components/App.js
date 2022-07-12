0e5161e93bdb8b94073f880cc27870f12a23f19f
Andrii Krupa <krupaandrii@gmail.com>
2022-07-12 15:57:25 +0300

    Friends section added

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import { Profile } from './Profile/Profile';
 import user from './data/user.json';
 import { Statistics } from './Statistics/Statistics';
 import data from './data/data.json';
+import { FriendList } from './FriendList/FriendList';
+import friends from './data/friends.json';
 
 export const App = () => {
   return (
@@ -16,6 +18,9 @@ export const App = () => {
       />
 
       <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+
+      <FriendList friends={friends} />
     </div>
   );
 };
