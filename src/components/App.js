292b14bf099b8a42081b5cfd6621865e396389ed
Andrii Krupa <krupaandrii@gmail.com>
2022-07-12 18:13:10 +0300

    Transactions History section added

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import { Statistics } from './Statistics/Statistics';
 import data from './data/data.json';
 import { FriendList } from './FriendList/FriendList';
 import friends from './data/friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from './data/transactions.json';
 
 export const App = () => {
   return (
@@ -16,11 +18,10 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
-
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
