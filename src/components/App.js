c27eaaa58d29a2c6b5ddd5f1bbcaa501d485772d
Andrii Krupa <krpaandrii@gmail.com>
2022-07-15 22:46:39 +0300

    Redone FriendsList component

--- src/components/App.js
+++ src/components/App.js
@@ -5,23 +5,23 @@ import { Statistics } from './Statistics/Statistics';
 import data from './data/data.json';
 import { FriendList } from './FriendList/FriendList';
 import friends from './data/friends.json';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import transactions from './data/transactions.json';
+// import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+// import transactions from './data/transactions.json';
 
 export const App = () => {
-  return (
-    <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />;
-    </div>
-  );
+   return (
+      <div>
+         <Profile
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            avatar={user.avatar}
+            stats={user.stats}
+         />
+         <Statistics title="Upload stats" stats={data} />
+         <Statistics stats={data} />
+         <FriendList friends={friends} />
+         {/* <TransactionHistory items={transactions} />; */}
+      </div>
+   );
 };
